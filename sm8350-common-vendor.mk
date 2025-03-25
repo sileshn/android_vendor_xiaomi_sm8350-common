d28524b75822195d5a97d58dd230a24292a22f00
Silesh K Nair <sileshn@gmail.com>
2025-03-25 03:59:34 +0000

    sm8350-common: commonize libcapiv2*

--- sm8350-common-vendor.mk
+++ sm8350-common-vendor.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
     libOpenCL \
     libadreno_utils \
     libc2d30_bltlib \
+    libcapiv2svacnn \
+    libcapiv2svarnn \
+    libcapiv2vop \
     libcdsprpc \
     libcheckpid \
     libconfigdb \
@@ -212,9 +215,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.1-impl \
     libbluetooth_audio_session_qti \
     libbluetooth_audio_session_qti_2_1 \
-    libcapiv2svacnn \
-    libcapiv2svarnn \
-    libcapiv2vop \
     libgcs-calwrapper \
     libgcs-ipc \
     libgcs-osal \
